bc7d744795fac10a401dd20a98920261daf9a061
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-11-12 15:29:36 +0100

    created js html generation loop

--- collection.js
+++ collection.js
@@ -4,7 +4,7 @@ const collection = [
         developer: 'Blizzard Entertainment',
         category: 'FPS',
         link: 'https://www.youtube.com/watch?v=MujtyCr3UjY',
-        description: 'Overwatch is a team-based FPS game published in 2016 in which the player can select a hero with its own unique gamestyle and abilities. Opposed to some other games of the same style, teammates can dynamically change heroes to adapt to what\'s needed in the game. Different modes are available, including competitive, classic games and arcade.',
+        description: 'Overwatch is a team-based FPS game published in 2016 in which the player can select a hero with its own unique gamestyle and abilities. Opposed to some other games of the same style, teammates can dynamically change heroes to adapt to what\'s needed in the game. Different modes are available, including competitive, classic games and arcade.'
     },
 
     {
@@ -12,7 +12,7 @@ const collection = [
         developer: 'Blizzard Entertainment',
         category: 'Digital collectible card games',
         link: 'https://www.youtube.com/watch?v=VCyzHULCf9k',
-        description: 'Hearthstone is a free-to-play collectible card game based on Blizzard\'s heroes of Warcraft. In this totally cross-platform card game (it is available on desktop, iOS and Android), you can play against friends or total strangers, in different modes ranging from regular one-on-one matches, ranked matches, to more arcade-like modes.',
+        description: 'Hearthstone is a free-to-play collectible card game based on Blizzard\'s heroes of Warcraft. In this totally cross-platform card game (it is available on desktop, iOS and Android), you can play against friends or total strangers, in different modes ranging from regular one-on-one matches, ranked matches, to more arcade-like modes.'
     },
 
     {
@@ -20,7 +20,7 @@ const collection = [
         developer: 'Nintendo',
         category: 'Action-Adventure',
         link: 'https://www.youtube.com/watch?v=6pA0Ia0G9Ac',
-        description: 'In this game of the series, you play an amnesic Link who just woke up after a hundred-years slumber caused by his last fight with Ganon. Your mission will be to find your memory back and travel across the kingdom of Hyrule to try and save the world and princess Zelda from Ganon.',
+        description: 'In this game of the series, you play an amnesic Link who just woke up after a hundred-years slumber caused by his last fight with Ganon. Your mission will be to find your memory back and travel across the kingdom of Hyrule to try and save the world and princess Zelda from Ganon.'
     },
 
     {
@@ -28,7 +28,7 @@ const collection = [
         developer: 'Rare',
         category: 'Action-Adventure',
         link: 'https://www.youtube.com/watch?v=r5JIBaasuE8',
-        description: 'Sea of Thieves is a multiplayer game where you can live your own pirate advanture, roaming the sea with up to 3 friends. On your journey, you\'ll look for treasures and battle with others on your way to become a Pirate Legend.',
+        description: 'Sea of Thieves is a multiplayer game where you can live your own pirate advanture, roaming the sea with up to 3 friends. On your journey, you\'ll look for treasures and battle with others on your way to become a Pirate Legend.'
     },
 
     {
@@ -36,7 +36,7 @@ const collection = [
         developer: 'Mojang Studios',
         category: ['Sandbox', 'Survival'],
         link: 'https://www.youtube.com/watch?v=MmB9b5njVbA',
-        description: 'In Minecraft, you explore a blocky, procedurally-generated 3D world with infinite terrain, and may discover and extract raw materials, craft tools and items, and build structures or earthworks.',
+        description: 'In Minecraft, you explore a blocky, procedurally-generated 3D world with infinite terrain, and may discover and extract raw materials, craft tools and items, and build structures or earthworks.'
     },
 
     {
@@ -44,15 +44,15 @@ const collection = [
         developer: 'EA Dice',
         category: ['FPS', 'Third Person Shooter'],
         link: 'https://www.youtube.com/watch?v=_q51LZ2HpbE',
-        description: 'This reboot of Star Wars Battlefront II has single-player and multiplayer game modes. Gameplay is split in two main categories: ground battles and space battles. Both occur across three eras covered by the three Star Wars Trilogies.',
+        description: 'This reboot of Star Wars Battlefront II has single-player and multiplayer game modes. Gameplay is split in two main categories: ground battles and space battles. Both occur across three eras covered by the three Star Wars Trilogies.'
     },
 
     {
         title: 'Guild Wars II',
         developer: 'ArenaNet',
         category: 'MMORPG',
-        link: ,
-        description: 'Set in the fantasy world of Tyria, the game follows the re-emergence of Destiny\'s Edge, a disbanded guild dedicated to fighting the Elder Dragons, a Lovecraftian species that has seized control of Tyria in the time since the original Guild Wars. The game takes place in a persistent world with a story that progresses in instanced environments.',
+        link: 'https://www.youtube.com/watch?v=Em0Sd60iI2w',
+        description: 'Set in the fantasy world of Tyria, the game follows the re-emergence of Destiny\'s Edge, a disbanded guild dedicated to fighting the Elder Dragons, a Lovecraftian species that has seized control of Tyria in the time since the original Guild Wars. The game takes place in a persistent world with a story that progresses in instanced environments.'
     },
 
     {
@@ -60,7 +60,7 @@ const collection = [
         developer: 'CD Projekt Red',
         category: ['Action role-playing', 'FPS'],
         link: 'https://www.youtube.com/watch?v=ixl31324UxE',
-        description: 'Adapted from the Cyberpunk franchise, the story takes place in dystopian Night City, an open world with six distinct regions. Players assume the first-person perspective of a customisable mercenary known as V, who can acquire skills in hacking and machinery, an arsenal of ranged weapons, and options for melee combat.',
+        description: 'Adapted from the Cyberpunk franchise, the story takes place in dystopian Night City, an open world with six distinct regions. Players assume the first-person perspective of a customisable mercenary known as V, who can acquire skills in hacking and machinery, an arsenal of ranged weapons, and options for melee combat.'
     },
 
     {
@@ -68,7 +68,7 @@ const collection = [
         developer: 'CD Projekt Red',
         category: 'Action role-playing',
         link: 'https://www.youtube.com/watch?v=TZ_G6XiHoUA',
-        description: 'The game takes place in a fictional fantasy world based on Slavonic mythology. Players control protagonist Geralt of Rivia, a monster slayer for hire known as a Witcher who is looking for his missing adopted daughter on the run from the Wild Hunt, an otherworldly force determined to capture her.',
+        description: 'The game takes place in a fictional fantasy world based on Slavonic mythology. Players control protagonist Geralt of Rivia, a monster slayer for hire known as a Witcher who is looking for his missing adopted daughter on the run from the Wild Hunt, an otherworldly force determined to capture her.'
     },
 
     {
@@ -76,6 +76,78 @@ const collection = [
         developer: 'Rockstar Studios',
         category: 'Action-Adventure',
         link: 'https://www.youtube.com/watch?v=F63h3v9QV7w',
-        description: 'The story is set in 1899 in a fictionalized representation of the Western, Midwestern, and Southern United States and follows outlaw Arthur Morgan, a member of the Van der Linde gang. Arthur must deal with the decline of the Wild West whilst attempting to survive against government forces, rival gangs, and other adversaries.',
-    },
-]
+        description: 'The story is set in 1899 in a fictionalized representation of the Western, Midwestern, and Southern United States and follows outlaw Arthur Morgan, a member of the Van der Linde gang. Arthur must deal with the decline of the Wild West whilst attempting to survive against government forces, rival gangs, and other adversaries.'
+    }
+];
+
+const columns = document.querySelector('.columns.is-mobile.is-multiline.is-marginless');
+columns.innerHTML = '';
+
+for(let element of collection){
+    const columnDiv = document.createElement("div");
+    columnDiv.className = "column is-full-mobile is-one-quarter-tablet is-one-quarter-desktop is-narrow";
+
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "card";
+
+    //Card image section
+    const cardImgDiv = document.createElement("div");
+    cardImgDiv.className = "card-image";
+
+    const cardImgFigure = document.createElement("figure");
+    cardImgFigure.className = "image is-4by3";
+
+       // const cardImg = document.createElement("img");
+       // cardImg.className = element.image
+
+    //Card content section
+    const cardContent = document.createElement("div");
+    cardContent.className = "card-content";
+    const tag = document.createElement("span");
+    tag.textContent = element.category;
+
+    const media = document.createElement("div");
+    media.className = "media";
+
+    const mediaContent = document.createElement("div");
+    mediaContent.className = "media-content";
+
+    const titleCard = document.createElement("p");
+    titleCard.className = "title is-4";
+    titleCard.textContent = element.title;
+
+    const dev = document.createElement("p");
+    dev.className = "subtitle is-6";
+    dev.textContent = element.developer;
+
+    const description = document.createElement('p');
+    description.className = "content";
+    description.textContent = element.description;
+
+    //footer
+
+    const cardFooter = document.createElement("footer");
+    cardFooter.className = "card-footer is-flex";
+
+    const linkFooter = document.createElement("a");
+    linkFooter.className = "card-footer-item";
+    linkFooter.textContent = element.link;
+
+    const fontAw = document.createElement("i");
+    fontAw.className = "fab fa-youtube fa-2x is-centered is-justify-content-center";
+
+    //Append child
+
+    // columns.is-mobile.is-multiline.is-marginless.appendChild(columnDiv);
+    columns.appendChild(columnDiv);
+    columnDiv.appendChild(cardDiv);
+    cardDiv.appendChild(cardImgDiv);
+    cardImgDiv.appendChild(cardImgFigure);
+    cardDiv.appendChild(cardContent);
+    cardContent.appendChild(tag);
+    cardContent.appendChild(media);
+    media.appendChild(mediaContent);
+    mediaContent.appendChild(titleCard);
+    mediaContent.appendChild(dev);
+    cardContent.appendChild(description);
+}
