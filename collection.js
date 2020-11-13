9e47a004105a92aa29100afc9a68bcdb5f165eb0
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-11-13 10:54:19 +0100

    Fixed element.category is not iterable + Deleted all HTML cards in index.html

--- collection.js
+++ collection.js
@@ -1,5 +1,4 @@
-const category = [
-    {
+const category = [{
         name: 'FPS',
         style: 'tag is-info m-1'
 
@@ -42,11 +41,10 @@ const category = [
     }
 ];
 
-const collection = [
-    {
+const collection = [{
         title: 'Overwatch',
         developer: 'Blizzard Entertainment',
-        category: category[0],
+        category: [category[0]],
         image: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Overwatch_cover_art.jpg/220px-Overwatch_cover_art.jpg',
         link: 'https://www.youtube.com/watch?v=MujtyCr3UjY',
         description: 'Overwatch is a team-based FPS game published in 2016 in which the player can select a hero with its own unique gamestyle and abilities. Opposed to some other games of the same style, teammates can dynamically change heroes to adapt to what\'s needed in the game. Different modes are available, including competitive, classic games and arcade.'
@@ -55,7 +53,7 @@ const collection = [
     {
         title: 'Hearthstone',
         developer: 'Blizzard Entertainment',
-        category: category[1],
+        category: [category[1]],
         image: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Hearthstone_2016_logo.png/220px-Hearthstone_2016_logo.png',
         link: 'https://www.youtube.com/watch?v=VCyzHULCf9k',
         description: 'Hearthstone is a free-to-play collectible card game based on Blizzard\'s heroes of Warcraft. In this totally cross-platform card game (it is available on desktop, iOS and Android), you can play against friends or total strangers, in different modes ranging from regular one-on-one matches, ranked matches, to more arcade-like modes.'
@@ -64,7 +62,7 @@ const collection = [
     {
         title: 'The Legend of Zelda: Breath of the Wild',
         developer: 'Nintendo',
-        category: 'category[2]',
+        category: [category[2]],
         image: 'https://www.nintendo.com/content/dam/noa/fr_CA/games/switch/t/the-legend-of-zelda-breath-of-the-wild-switch/the-legend-of-zelda-breath-of-the-wild-switch-hero.jpg',
         link: 'https://www.youtube.com/watch?v=6pA0Ia0G9Ac',
         description: 'In this game of the series, you play an amnesic Link who just woke up after a hundred-years slumber caused by his last fight with Ganon. Your mission will be to find your memory back and travel across the kingdom of Hyrule to try and save the world and princess Zelda from Ganon.'
@@ -72,9 +70,9 @@ const collection = [
 
     {
         title: 'Sea of Thieves',
-        developer: Rare,
-        category: category[2],
-        image:'https://upload.wikimedia.org/wikipedia/en/7/77/Sea_of_thieves_cover_art.jpg',
+        developer: 'Rare',
+        category: [category[2]],
+        image: 'https://upload.wikimedia.org/wikipedia/en/7/77/Sea_of_thieves_cover_art.jpg',
         link: 'https://www.youtube.com/watch?v=r5JIBaasuE8',
         description: 'Sea of Thieves is a multiplayer game where you can live your own pirate advanture, roaming the sea with up to 3 friends. On your journey, you\'ll look for treasures and battle with others on your way to become a Pirate Legend.'
     },
@@ -100,7 +98,7 @@ const collection = [
     {
         title: 'Guild Wars II',
         developer: 'ArenaNet',
-        category: category[6],
+        category: [category[6]],
         image: 'https://image.jeuxvideo.com/images/jaquettes/00018092/jaquette-guild-wars-2-pc-cover-avant-g-1344324878.jpg',
         link: 'https://www.youtube.com/watch?v=Em0Sd60iI2w',
         description: 'Set in the fantasy world of Tyria, the game follows the re-emergence of Destiny\'s Edge, a disbanded guild dedicated to fighting the Elder Dragons, a Lovecraftian species that has seized control of Tyria in the time since the original Guild Wars. The game takes place in a persistent world with a story that progresses in instanced environments.'
@@ -118,7 +116,7 @@ const collection = [
     {
         title: 'The Witcher 3 : Wild Hunt',
         developer: 'CD Projekt Red',
-        category: category[7],
+        category: [category[7]],
         image: 'https://upload.wikimedia.org/wikipedia/en/0/0c/Witcher_3_cover_art.jpg',
         link: 'https://www.youtube.com/watch?v=TZ_G6XiHoUA',
         description: 'The game takes place in a fictional fantasy world based on Slavonic mythology. Players control protagonist Geralt of Rivia, a monster slayer for hire known as a Witcher who is looking for his missing adopted daughter on the run from the Wild Hunt, an otherworldly force determined to capture her.'
@@ -127,7 +125,7 @@ const collection = [
     {
         title: 'Red Dead Redemption II',
         developer: 'Rockstar Studios',
-        category: category[2],
+        category: [category[2]],
         image: 'https://upload.wikimedia.org/wikipedia/en/4/44/Red_Dead_Redemption_II.jpg',
         link: 'https://www.youtube.com/watch?v=F63h3v9QV7w',
         description: 'The story is set in 1899 in a fictionalized representation of the Western, Midwestern, and Southern United States and follows outlaw Arthur Morgan, a member of the Van der Linde gang. Arthur must deal with the decline of the Wild West whilst attempting to survive against government forces, rival gangs, and other adversaries.'
@@ -136,10 +134,10 @@ const collection = [
 
 // HTML modification script
 
-const columns = document.querySelector('.columns.is-mobile.is-multiline.is-marginless');
-columns.innerHTML = '';
+const columns = document.querySelector(".columns.is-mobile.is-multiline.is-marginless");
+columns.innerHTML = "";
 
-for(let element of collection){
+for (let element of collection) {
     const columnDiv = document.createElement("div");
     columnDiv.className = "column is-full-mobile is-one-quarter-tablet is-one-quarter-desktop is-narrow";
 
@@ -160,12 +158,12 @@ for(let element of collection){
     const cardContent = document.createElement("div");
     cardContent.className = "card-content";
 
-    // for(let elem of element.category){
-    //     const tag = document.createElement("span");
-    //     tag.textContent= elem.name;
-    //     tag.className = elem.style;
-    //     cardContent.appendChild(tag);
-    // }
+    for (let elem of element.category) {
+        const tag = document.createElement("span");
+        tag.textContent = elem.name;
+        tag.className = elem.style;
+        cardContent.appendChild(tag);
+    }
 
     const media = document.createElement("div");
     media.className = "media";
@@ -194,7 +192,7 @@ for(let element of collection){
     const linkFooter = document.createElement("a");
     linkFooter.className = "card-footer-item";
     linkFooter.src = element.link;
-    linkFooter.target= "_blank";
+    linkFooter.target = "_blank";
 
     const fontAw = document.createElement("i");
     fontAw.className = "fab fa-youtube fa-2x is-centered is-justify-content-center";
